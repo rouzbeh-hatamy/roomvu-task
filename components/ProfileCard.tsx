eb5c626ed7fdb31e490433abd0519151b75c7e42
rouzbeh <roozbeh.hatamy@gmail.com>
2023-08-19 02:29:26 +0330

    add main config

---
+++ components/ProfileCard.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function ProfileCard() {
+    return (
+        <div className="flex items-center p-4 space-x-4 bg-white rounded-lg">
+            <div className="w-16 h-16">
+                <Image
+                    src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
+                    alt="Profile Picture"
+                    width={64}
+                    height={64}
+                    className="rounded-full"
+                />
+            </div>
+            <div>
+                <span className="text-lg  text-gray-800">Personal blog by <Link href="https://mobile.twitter.com/dan_abramov" className='text-primary underline hover:no-underline'>Dan Abramov.</Link></span>
+                <p className="text-gray-800 text-lg ">I explain with words and code.</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default ProfileCard
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+      domains: ['overreacted.io'],
+  },
 }
 
 module.exports = nextConfig
